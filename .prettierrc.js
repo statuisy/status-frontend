f0f046464b69c01c9943ba70bb1983f4e533f99c
rvonhoog <rvonhoog@pm.me>
2020-01-05 11:22:54 -0800

    🎉Init ✨ Basic setup. 🔥 removed all DJs dotfiles.➕ nextjs, react, react-dom

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "endOfLine": "lf",
-  "tabWidth": 2,
-  "arrowParens": "avoid",
-  "printWidth": 100,
-  "semi": false,
-  "singleQuote": true,
-}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+	<div>
+		<p>Status Page™️ </p>
+	</div>
+)
+
+export default Index;
