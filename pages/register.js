7e247feaad36ba252fd7f4d267f1e04703ddea59
Michael Jivan <michaeljivan@Michaels-MacBook-Pro-2.local>
2020-01-10 05:26:49 -0800

    branch mike

---
+++ pages/register.js
@@ -0,0 +1,9 @@
+const Register = () => (
+    <div>
+        <label>Registration Form</label><br />
+        <input></input> <br />
+        <input></input>
+    </div>
+)
+
+export default Register;
