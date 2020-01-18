ded5ee5b416f47cde5b7f3ce26bb46396532fbb6
rvonhoog <rvonhoog@pm.me>
2020-01-18 08:55:44 -0800

    ✨ Added Maintenance Details Page.

--- components/Maintenance.js
+++ components/Maintenance.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import DueIn from './DueIn';
+import Link from 'next/link';
 
 import data from '../data/maintenance.json';
 
 const Maintenance = () => {
+
 	return (
 		<div className="container">
 			<label>Maintenance</label>
 			{data.map((item) => {
 				return (
-					<div key={item.id}>
+					<Link href={`/maintenance?eid=${item.id}`} key={item.id} >
 						<div className="alert alert-primary">
-							{item.title}<br />
-							{item.description}<br />
-							<DueIn howLong={item.how_long} createdAt={item.created_at} status={item.status} />
+							<p>{item.title}</p>
+							<p>{item.description}</p>
+							<DueIn 
+								howLong={item.how_long} 
+								createdAt={item.created_at} 
+								status={item.status} 
+							/>
 						</div>
-					</div>
+					</Link>
 				)
 			})}
 		</div>
