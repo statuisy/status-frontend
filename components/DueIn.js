c8ac18357951b5a9158c0e77d02d45ec920bead4
rvonhoog <rvonhoog@pm.me>
2020-01-18 10:09:35 -0800

    ✨ Made Utils.js with timeConversion and Prettydate. ♻️

--- components/DueIn.js
+++ components/DueIn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { timeConversion } from './Utils';
 
 const DueIn = (props ) => {
 	const now = new Date();
@@ -10,26 +11,6 @@ const DueIn = (props ) => {
 	//If Maintentance is completed return blank
 	if(props.status === 'completed'){
 		return (<></>)
-	}
-	function timeConversion(millisec) {
-
-        	var seconds = (millisec / 1000).toFixed(1);
-
-        	var minutes = (millisec / (1000 * 60)).toFixed(1);
-
-        	var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-
-        	var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
-        	if (seconds < 60) {
-        	    return seconds + " Sec";
-        	} else if (minutes < 60) {
-        	    return minutes + " Min";
-        	} else if (hours < 24) {
-        	    return hours + " Hrs";
-        	} else {
-        	    return days + " Days"
-        	}
 	}
 
 	if(now < due){
--- pages/maintenance.js
+++ pages/maintenance.js
@@ -4,6 +4,7 @@ import data from '../data/maintenance.json';
 import { useRouter } from 'next/router';
 import PageHeader from '../components/PageHeader';
 import Footer from '../components/Footer';
+import { timeConversion, prettyDate } from '../components/Utils';
 
 const MaintenancePage = props => {
 	//Get ID from query string using next/router
@@ -14,46 +15,6 @@ const MaintenancePage = props => {
 	//Find the entry from data by ID
 	const entry = data.find(item => item.id === id);
 	//console.log(entry);
-
-	 function timeConversion(millisec) {
-
-                var seconds = (millisec / 1000).toFixed(1);
-
-                var minutes = (millisec / (1000 * 60)).toFixed(1);
-
-                var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-
-                var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
-                if (seconds < 60) {
-                    return seconds + " Sec";
-                } else if (minutes < 60) {
-                    return minutes + " Min";
-                } else if (hours < 24) {
-                    return hours + " Hrs";
-                } else {
-                    return days + " Days"
-                }
-        }
-
-	function prettyDate(dateAsString) {
-		const date = new Date(dateAsString);
-		console.log(date);
-
-		const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-		const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-		return (
-			days[date.getDay()] + ' ' + 
-			date.getDate() + ' ' + 
-			months[date.getMonth()] + ' ' + 
-			date.getFullYear() + 
-			' at ' + 
-			date.getHours() + 
-			':' + 
-			(date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMintues())
-		);
-
-	}
 
 	 
 	return (
